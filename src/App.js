5ca9f83c4a9542c07e8ce2bd09d44432ac51e6c0
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-06 13:23:09 -0600

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
     <div className="App">
       <Navigation/>
-      <Logo/>
+      {<Logo/>}
       {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,9 @@ import React from "react";
 const Navigation = () => {
   return (
     <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+      <p className='f3 link dim black underline pa3 pointer'>
+        Sign Out
+      </p>
     </nav>
   );
 }
